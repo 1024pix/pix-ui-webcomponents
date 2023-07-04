c0f3435f9c9dd583b51727b29dd587095eeebd33
Clarisse Damon <clarisse.damon@pix.fr>
2023-07-04 11:57:40 +0200

    add colors button story

--- style/stories/Button.stories.js
+++ style/stories/Button.stories.js
@@ -15,3 +15,43 @@ export const Default = {
     label: "Default button",
   },
 };
+
+export const Colors = {
+  title: "Buttons",
+  render: ({ label, ...args }) => {
+    return `<div style="display: flex; gap: 2rem; flex-wrap: wrap"><button
+      type="button"
+      class="pix-button"
+    >
+      Bouton default
+    </button>
+    <button
+      type="button"
+      class="pix-button pix-button--background-yellow"
+    >
+      Bouton jaune
+    </button>
+    <button
+      type="button"
+      class="pix-button pix-button--background-red"
+    >
+      Bouton rouge
+    </button>
+    <button
+      type="button"
+      class="pix-button pix-button--background-green"
+    >
+     Bouton vert
+    </button>
+    <button
+      type="button"
+      class="pix-button pix-button--background-grey"
+    >
+    Bouton gris
+    </button>
+    </div>`;
+  },
+  args: {
+    label: "Bouton"
+  }
+};
