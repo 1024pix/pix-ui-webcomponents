ddcf808d4bc88545f8f9764bb7ba7d6d47ecb454
Rébecca <rebecca.kaci@gmail.com>
2023-07-05 14:47:31 +0200

    create pix-button component

---
+++ src/components/pix-button/pix-button.tsx
@@ -0,0 +1,32 @@
+import { Component, Prop, h } from '@stencil/core';
+import { format } from '../../utils/utils';
+
+@Component({
+  tag: 'pix-button',
+  styleUrl: 'pix-button.css',
+  shadow: true,
+})
+export class PixButton {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return format(this.first, this.middle, this.last);
+  }
+
+  render() {
+    return <button>HelloPix {this.getText()}</button>;
+  }
+}
