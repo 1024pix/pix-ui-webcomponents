d387d8b0ac09d20ec1d3159594408642f530fe31
LEGO Technix <109212476+lego-technix@users.noreply.github.com>
2023-07-24 18:39:39 +0200

    Dispatch submit event on host form
    Co-authored-by: LEGO Technix <109212476+lego-technix@users.noreply.github.com>
    Co-authored-by: BKsensei <rebecca.kaci@gmail.com>
    Co-authored-by: Iris Benoit <iris.benoit@pix.fr>
    Co-authored-by: Jérémie Jadé <jeremie.jade@pix.fr>

--- src/components/PixButton.ce.vue
+++ src/components/PixButton.ce.vue
@@ -1,14 +1,26 @@
 <script setup>
+import {ref} from 'vue'
+
+const button = ref()
+
 defineProps({
   type: {
     type: String,
     //required: true,
   },
 })
+
+function triggerAction(event) {
+  console.log('triggerAction')
+  button.value.getRootNode().host.closest('form')
+    .dispatchEvent(new Event('submit', {cancelable: true}))
+  //button.value.dispatchEvent(new Event('submit', {cancelable: true, composed: true}))
+  console.log('Done')
+}
 </script>
 
 <template>
-  <button v-bind:type="type">
+  <button ref="button" v-bind:type="type" @click="triggerAction">
     <slot></slot>
   </button>
 </template>
